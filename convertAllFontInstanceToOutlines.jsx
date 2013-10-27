539e1bae26be44db18556e4cd28724836a16bee9
Mark Kennedy <mark@verynoisy.com>
2013-10-27 19:41:49 +0000

    tool for creating outlines of dingbat fonts

---
+++ convertAllFontInstanceToOutlines.jsx
@@ -0,0 +1,18 @@
+﻿main()
+function main() {
+    replaceTextByFont( "Martin");
+    replaceTextByFont( "XBOX-360");
+    replaceTextByFont( "PrimaDingbatsWii");
+}
+
+function replaceTextByFont( dafont ) {
+    app.findTextPreferences = app.changeTextPreferences = NothingEnum.nothing;
+    app.findTextPreferences.appliedFont = app.fonts.item(dafont);
+    app.changeTextPreferences.appliedFont = app.fonts.item("Arial");
+    chars = app.activeDocument.findText();
+    for( i = 0; i < chars.length; i ++ ) {
+        chars[i].createOutlines(true);
+        //chars[i]
+    }
+    chars = app.activeDocument.changeText();
+}
