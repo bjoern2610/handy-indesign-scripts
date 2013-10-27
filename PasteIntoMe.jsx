de096572c4d9d521ca74b1b13c990d3ccd923196
Mark Kennedy <mark@verynoisy.com>
2013-10-27 17:22:46 +0000

    PasteIntoMe quickie thing

---
+++ PasteIntoMe.jsx
@@ -0,0 +1,18 @@
+﻿//PasteIntoHack.jsx
+//An InDesign JavaScript
+/*  
+@@@BUILDINFO@@@ "PasteIntoHack.jsx" 1.0.0 16 October 2013
+*/
+// Groups a selection, cuts it, creates a frame, pastes group into frame, then autosizes frame.
+//
+main();
+function main() {
+    var myObj = new Array;
+    myObj = app.selection;
+    if( myObj.length == 1 ) {
+        g = myObj[0];
+        g.select();
+        app.copy();
+        app.pasteInto();
+    }
+}
